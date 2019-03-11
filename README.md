482167773240b480f16ecb57374265a97209945c
Mark Guildea <mark.anthony.guildea@gmail.com>
2019-03-11 22:31:35 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Trying a different layout 
+
+One site per page
+
+Coldroom on one with query selector used to change 
+
---
+++ static/js/main_3.js
@@ -0,0 +1,7 @@
+// onclick changes 
+
+$(".btn_Cr").click(function(){
+    $(".trend_1").css("display", "none"),
+    $(".trend_2").css("display", "block")
+    
+})
